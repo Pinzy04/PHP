879923e01f5896f5dcabba5090b6c07e7f9c73d5
Pinzy <94610807+Pinzy04@users.noreply.github.com>
2022-10-17 23:10:20 +0200

    Update bottoni-a-scomparsa.php

--- bottoni-a-scomparsa/bottoni-a-scomparsa.php
+++ bottoni-a-scomparsa/bottoni-a-scomparsa.php
@@ -1,15 +1,18 @@
 <?php
     $r = 4;
     $c = 5;
+
     for($i=0; $i<$r; $i++){
         for($j=0; $j<$c; $j++){
-            $t[$i][$j] = "<INPUT type='submit' name='T[".$i."][".$j."]' value='&nbsp;' size='1'/>";
-        }
-    }
-    for($i=0; $i<$r; $i++){
-        for($j=0; $j<$c; $j++){
-            if(isset($_POST['T['.$i.']['.$j.']'])){
-                $t[$i][$j] = "<p></p>";
+            if(isset($_POST["T$i$j"])){
+                $t[$i][$j] = "<INPUT type='hidden' name='T$i$j' value='&nbsp;' size='1'/>";
+            }
+            else{
+                $t[$i][$j] = "<INPUT type='submit' name='T$i$j' value='&nbsp;' size='1'/>";
+            }
+            
+            if(isset($_POST["R"])){
+                $t[$i][$j] = "<INPUT type='submit' name='T$i$j' value='&nbsp;' size='1'/>";
             }
         }
     }
@@ -31,6 +34,8 @@
                 }		  
                 echo "</TABLE>";
             ?>
+            <br>
+            <input type='submit' name='R' value='Reset'/>
         </form>
     </body>
 </html>
