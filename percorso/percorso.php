012cb4057937cca746ef537797146faf20fcc5e4
Pinzy <94610807+Pinzy04@users.noreply.github.com>
2022-10-20 13:43:33 +0200

    Update percorso.php

--- percorso/percorso.php
+++ percorso/percorso.php
@@ -1,39 +1,134 @@
 <?php
+    session_start();
+
+    // righe e colonne della matrice
     $r = 9;
     $c = 9;
+    $v = 4;
+    $o = 4;
 
-    for($i=0; $i<$r; $i++){
-        for($j=0; $j<$c; $j++){
-            if(isset($_POST["T$i$j"])){
-                $_SESSION[$t[$i][$j]] = "<td><INPUT type='hidden' name='T$i$j' value='&nbsp;' size='1'></td>";
+    if(isset($_POST['R']))
+    {
+        $_SESSION['valori']=null;
+    }
+
+    if(!isset($_SESSION['valori']))
+    {
+        $_SESSION['valori'] = Array();
+        for($i=0; $i<$r; $i++)
+        {
+            for($j=0; $j<$c; $j++)
+            {   
+                if($i == 4 && $j == 4)
+                {
+                    $_SESSION['valori'][$i][$j] = 1;
+                }
+                else
+                {
+                    $_SESSION['valori'][$i][$j] = 0;
+                }
             }
-            else{
-                $_SESSION[$t[$i][$j]] = "<td><INPUT type='submit' name='T$i$j' value='&nbsp;' size='1'></td>";
-            }
-            
-            if(isset($_POST["R"])){
-                $_SESSION[$t[$i][$j]] = "<td><INPUT type='submit' name='T$i$j' value='&nbsp;' size='1'></td>";
-            }
+        }
+    }
+
+    if(isset($_POST['N']))
+    {
+        for($i=0; $i<$r; $i++)
+        {
+            for($j=0; $j<$c; $j++)
+            {
+                $v++;
+                $_SESSION['valori'][$o][$v] = 1;
+            } 
+        }
+    }
+
+    if(isset($_POST['O']))
+    {
+        for($i=0; $i<$r; $i++)
+        {
+            for($j=0; $j<$c; $j++)
+            {
+                $o--;
+                $_SESSION['valori'][$o][$v] = 1;
+            } 
+        }
+    }
+
+    if(isset($_POST['E']))
+    {
+        for($i=0; $i<$r; $i++)
+        {
+            for($j=0; $j<$c; $j++)
+            {
+                $o++;
+                $_SESSION['valori'][$o][$v] = 1;
+            } 
+        }
+    }
+
+    if(isset($_POST['S']))
+    {
+        for($i=0; $i<$r; $i++)
+        {
+            for($j=0; $j<$c; $j++)
+            {
+                $v--;
+                $_SESSION['valori'][$o][$v] = 1;
+            } 
         }
     }
 ?>
 
 <html>
-    <body align='center'>
+   <body align='center' style="text-align: center; background: #6495ED">
+
+        <!-- viene creato il FORM per gestire la matrice di text -->
         <form name='F1' method='POST' action='<?php echo $_SERVER['PHP_SELF']?>'>
+
             <?php
-                echo "<TABLE border='1' align='center'>";
-                for($i=0; $i<$r; $i++){
-                    echo "<TR>"; 
-                    for($j=0; $j<$c; $j++){
-                        echo $t[$i][$j]; 
+                // tabella HTML contenente la matrice di submit
+                echo "<table border='1' align='center'>";
+                for($i=0; $i<$r; $i++)
+                {
+                    echo "<tr>"; 
+                    for($j=0; $j<$c; $j++)
+                    {
+                        if($i == $o && $j == $v)
+                        {
+                            echo "<td style='background: red'> &nbsp;&nbsp;&nbsp;&nbsp; </td>";
+                        }
+                        else
+                        {
+                            echo "<td style='background: white'> &nbsp;&nbsp;&nbsp;&nbsp; </td>"; 
+                        }
+                        
                     }		  
-                    echo "</TR>"; 
+                    echo "</tr>"; 
                 }		  
-                echo "</TABLE>";
+                echo "</table>";
+                // fine tabella HTML
             ?>
-            <br>
-            <input type='submit' name='R' value='Reset'/>
+            
+            <table border='1' align='center'>
+                <tr>
+                    <td style='background: white'> &nbsp;&nbsp;&nbsp;&nbsp; </td>
+                    <td><input type='submit' name='N' value='N' /></td>
+                    <td style='background: white'> &nbsp;&nbsp;&nbsp;&nbsp; </td>
+                </tr>
+                <tr>
+                    <td><input type='submit' name='O' value='O' /></td>
+                    <td><input type='submit' name='R' value='R' /></td>
+                    <td><input type='submit' name='E' value='E' /></td>
+                </tr>
+                    <td style='background: white'> &nbsp;&nbsp;&nbsp;&nbsp; </td>
+                    <td><input type='submit' name='S' value='S' /></td>
+                    <td style='background: white'> &nbsp;&nbsp;&nbsp;&nbsp; </td>
+                </tr>
+            </table>
         </form>
+
+        <br><br>
+
     </body>
 </html>
