92ef677522eeb2e6fa9f230834d39f639ebe6d57
Pinzy <94610807+Pinzy04@users.noreply.github.com>
2022-10-27 01:39:12 +0200

    Update bottoni-a-scomparsa.php

--- bottoni-a-scomparsa/bottoni-a-scomparsa.php
+++ bottoni-a-scomparsa/bottoni-a-scomparsa.php
@@ -1,114 +1,41 @@
+<?php
+    $r = 4;
+    $c = 5;
+
+    for($i=0; $i<$r; $i++){
+        for($j=0; $j<$c; $j++){
+            if(isset($_POST["T$i$j"])){
+                $t[$i][$j] = "<INPUT type='hidden' name='T$i$j' value='&nbsp;' size='1'/>";
+            }
+            else{
+                $t[$i][$j] = "<INPUT type='submit' name='T$i$j' value='&nbsp;' size='1'/>";
+            }
+            
+            if(isset($_POST["R"])){
+                $t[$i][$j] = "<INPUT type='submit' name='T$i$j' value='&nbsp;' size='1'/>";
+            }
+        }
+    }
+?>
+
 <html>
-   <head>
-      <title> Percorso </title>
-   </head>
-
-   <body align='center' style='background-color: gainsboro'>
-      <?php
-         session_start();
-         
-         if(isset($_POST['R']))
-         {
-            $_SESSION['y'] = null;
-            $_SESSION['x'] = null;
-            $_SESSION['mosse'] = null;
-         }
-
-         if(!isset($_SESSION['y']))
-            $_SESSION['y'] = 4;
-         if(!isset($_SESSION['x']))
-            $_SESSION['x'] = 4;
-         if(!isset($_SESSION['mosse']))
-            $_SESSION['mosse'] = 0;
-
-         function disegnaTable($ri,$co,$to,$ul)
-         {
-            echo "<table border='1' align='center'>";
-            for($i = 0; $i < 9; $i++)
-            {
-               echo "<TR>";
-               for($j=0;$j<9;$j++)
-               {
-                  if(($ri == $i) && ($co == $j))
-                     echo "<td bgcolor='red'>&nbsp;&nbsp;&nbsp;&nbsp;</td>";
-                  else
-                     echo "<td>&nbsp;&nbsp;&nbsp;&nbsp;</td>";
-               }
-               echo "</tr>";
-            }
-            echo "</table>";
-            echo "<BR>Ultima mossa:<B>".$ul."</B>";
-            echo "<BR>Totale mosse:<B>".$to."</B>";
-         }
-
-
-         $r = $_SESSION['y'];
-         $c = $_SESSION['x'];
-         $t = $_SESSION['mosse'];
-         $u = "NULLA";
-
-         if(isset($_POST['N']))
-         {
-            if($r > 0)
-            {
-               $r--;
-               $t++;
-               $u = "NORD";
-            }
-         }
-         if(isset($_POST['E']))
-         {
-            if($c < 8)
-            {
-               $c++;
-               $t++;
-               $u = "EST";
-            }
-         }
-         if(isset($_POST['S']))
-         {
-            if($r < 8)
-            {
-               $r++;
-               $t++;
-               $u = "SUD";
-            }
-         }
-         if(isset($_POST['O']))
-         {
-            if($c > 0)
-            {
-               $c--;
-               $t++;
-               $u = "OVEST";
-            }
-         }
-
-         disegnaTable($r, $c, $t, $u);
-
-         $_SESSION['y'] = $r;
-         $_SESSION['x'] = $c;
-         $_SESSION['mosse'] = $t;
-      ?>
-      <br><br>
-      <form name='F1' method='post' action='<?php echo $_SERVER['PHP_SELF']?>'>
-         <table border='1' align='center'>
-            <tr>
-                  <td>&nbsp;</td>
-                  <td><input type='submit' name='N' value='N'></td>
-                  <td>&nbsp;</td>
-            </tr>
-            <tr>
-                  <td><input type='submit' name='O' value='O'></td>
-                  <td><input type='submit' name='R' value='R'></td>
-                  <td><input type='submit' name='E' value='E'></td>
-            </tr>
-            <tr>
-                  <td>&nbsp;</td>
-                  <td><input type='submit' name='S' value='S'></td>
-                  <td>&nbsp;</td>
-            </tr>
-         </table>
-      </form>
-   </body>
+    <body>
+        <form name='F1' method='POST' action='<?php echo $_SERVER['PHP_SELF']?>'>
+            <?php
+                echo "<TABLE border='1'>";
+                for($i=0; $i<$r; $i++){
+                    echo "<TR>"; 
+                    for($j=0; $j<$c; $j++){
+                        echo "<TD>";
+                        echo $t[$i][$j];
+                        echo "</TD>"; 
+                    }		  
+                    echo "</TR>"; 
+                }		  
+                echo "</TABLE>";
+            ?>
+            <br>
+            <input type='submit' name='R' value='Reset'/>
+        </form>
+    </body>
 </html>
